c0abcfb1491600ca7c27ba1befc0c30c444b233f
Rafael Pereira <rafaelhpmg@gmail.com>
2025-02-20 02:21:39 -0300

    feat: add infrastructure layer and user schema/repository

---
+++ src/infra/repositories/user-repository.impl.ts
@@ -0,0 +1,32 @@
+import { User } from "@domain/entities/user.entity";
+import { UserRepository } from "@domain/repositories/user.repository";
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class UserRepositoryImpl implements UserRepository {
+  constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {}
+
+  async findByEmail(email: string): Promise<User | null> {
+    return await this.userRepository.findOneBy({ email });
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return await this.userRepository.findOneBy({ id });
+  }
+
+  async create(user: User): Promise<User> {
+    return await this.userRepository.save(user);
+  }
+
+  async update(user: User): Promise<User> {
+    const existingUser = await this.findById(user.id);
+
+    if (!existingUser) {
+      throw new Error('User not found');
+    }
+
+    return await this.userRepository.save(user);
+  }
+}
---
+++ src/infra/schemas/user.schema.ts
@@ -0,0 +1,35 @@
+import { EntitySchema } from 'typeorm';
+import { User } from '@domain/entities/user.entity';
+
+export const UserSchema = new EntitySchema<User>({
+  name: 'User',
+  target: User,
+  columns: {
+    id: {
+      type: 'uuid',
+      default: () => 'gen_random_uuid()',
+      primary: true,
+    },
+    email: {
+      type: 'varchar',
+      unique: true,
+      nullable: false,
+      length: 255,
+    },
+    password: {
+      type: 'varchar',
+      nullable: false,
+      length: 255,
+    },
+    createdAt: {
+      type: 'timestamp',
+      default: () => 'CURRENT_TIMESTAMP',
+      createDate: true,
+    },
+    updatedAt: {
+      type: 'timestamp',
+      default: () => 'CURRENT_TIMESTAMP',
+      updateDate: true,
+    },
+  },
+});
