8290f51492578e18809fc0a3259712a0f7f4f831
Rafael Pereira <rafaelhpmg@gmail.com>
2025-02-20 14:17:30 -0300

    feat: add ShortenedUrl entity to domain layer

---
+++ src/domain/entities/shortened-url.entity.ts
@@ -0,0 +1,36 @@
+import { User } from "./user.entity";
+
+export class ShortenedUrl {
+  id: string;
+  originalUrl: string;
+  shortUrl: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date;
+  userId: string | null;
+  user: User | null;
+
+  constructor(baseUrl: string) {
+    this.shortUrl = this.generateShortUrl(baseUrl);
+  }
+
+  private generateShortCode(): string {
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const charsLength = chars.length;
+    const codeLength = 6;
+    let result = '';
+  
+    const randomValues = new Uint32Array(codeLength);
+    crypto.getRandomValues(randomValues);
+      
+    for (let i = 0; i < codeLength; i++) {
+      result += chars[randomValues[i] % charsLength];
+    }
+  
+    return result;
+  }
+
+  public generateShortUrl(baseUrl: string): string {
+    return `${baseUrl}/${this.generateShortCode()}`;
+  }
+}
