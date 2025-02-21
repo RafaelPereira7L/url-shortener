0614e05556d1e4b562148262c4002df6b6936611
Rafael Pereira <rafaelhpmg@gmail.com>
2025-02-21 18:12:43 -0300

    chore: configure Docker and docker-compose for dev and prod environments

--- src/main.ts
+++ src/main.ts
@@ -7,26 +7,28 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  const config = new DocumentBuilder()
-    .setTitle('URL Shortener API')
-    .setDescription('The URL Shortener API description')
-    .setVersion('1.0')
-    .addTag('URL Shortener')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        name: 'JWT',
-        description: 'Enter JWT token',
-        in: 'header',
-      },
-      'JWT',
-    )
-    .build();
+  if (process.env.NODE_ENV !== 'PRODUCTION') {
+    const config = new DocumentBuilder()
+      .setTitle('URL Shortener API')
+      .setDescription('The URL Shortener API description')
+      .setVersion('1.0')
+      .addTag('URL Shortener')
+      .addBearerAuth(
+        {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+          name: 'JWT',
+          description: 'Enter JWT token',
+          in: 'header',
+        },
+        'JWT',
+      )
+      .build();
 
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+  }
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
